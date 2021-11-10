b4b08dd2a5b2d1551a88ed8d4290d5c0343595f8
Filip Katulski <filip.katulski@cech.ch>
2021-11-10 17:29:23 +0100

    Modified knb script to support different kubeconfig filepaths, started autotester python script

--- autotester.py
+++ autotester.py
@@ -1,9 +1,32 @@
-import json 
+import os
+import sys
+import getopt 
+import subprocess
+import json
+import yaml
 
-def read_data(filename: str):
-    f = open(filename)
-    data = json.load(f)
-    
+
+def run_command():
+    ...
+
+def parse_yaml():
+    ...
+
+
+def main (argv):
+    inputfile = ''
+
+    try:
+        opts, args = getopt.getopt(argv, "hi:",["help", "input-file"])
+    except getopt.GetoptError:
+        print('autotester.py failed to parse arguments.\n')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print("TODO help here")
+            sys.exit()
+        elif opt in ('-i', '--input-file'):
+            
 
 if __main__ == "__main__":
-    print
+    main(sys.argv[1:])
