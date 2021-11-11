84c720b610b50e680585abd0502d9326ad000804
Filip Katulski <filip.katulski@cech.ch>
2021-11-11 17:22:35 +0100

    Created scaffold for test yaml file, further work on autotester

--- autotester.py
+++ autotester.py
@@ -3,14 +3,23 @@ import sys
 import getopt 
 import subprocess
 import json
-import yaml
+import yaml 
 
 
-def run_command():
+def run_command(data: dict):
+    for key, value in data.items():
+        print(key, ' : ', value)
     ...
 
-def parse_yaml():
-    ...
+def parse_yaml(filepath: str) -> dict:
+    with open(filepath, "r") as stream:
+        try:
+            data = yaml.safe_load(stream)
+            print(data)
+            print(type(data))
+        except yaml.YAMLError as yerr:
+            print(yerr)
+    return data
 
 
 def main (argv):
@@ -26,7 +35,12 @@ def main (argv):
             print("TODO help here")
             sys.exit()
         elif opt in ('-i', '--input-file'):
-            
+            inputfile = arg 
+    
+    print('Input file is "',inputfile, '"')
+    test_data = parse_yaml(inputfile)
+    run_command(test_data)
 
-if __main__ == "__main__":
+
+if __name__ == "__main__":
     main(sys.argv[1:])
